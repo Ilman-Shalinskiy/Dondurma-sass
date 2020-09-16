1d503a8c0734d2e51e97c2f039318cb5ad5bf701
Ilman-Shalinskiy <60791812+Ilman-Shalinskiy@users.noreply.github.com>
2020-09-16 16:36:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,21 @@
-$(document).ready(function(){
-    $('.location__carousel').slick({
-        speed: 1400,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><span>&#8249;</span></button>',
-        nextArrow: '<button type="button" class="slick-next"><span>&#8250;</span></button>',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            speed: 500,
-            arrows: false,
-          }
-        }
-      ]
-    });
-  });
-
-
 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    bodyActive = document.querySelector('body');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('is-active');
         menu.classList.toggle('menu_active');
+        bodyActive.classList.toggle('body-hidden');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('is-active');
             menu.classList.toggle('menu_active');
-        })
-    })
-})
+            bodyActive.classList.toggle('body-hidden');
+        });
+    });
+});
